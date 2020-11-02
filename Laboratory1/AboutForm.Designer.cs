f3fd033dd653434c82b8d664b072fad9b0ec10c4
Timur <timur.hiptenko@gmail.com>
2020-11-03 00:28:39 +0200

    Add files via upload

---
+++ Laboratory1/AboutForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Laboratory1
+{
+	partial class AboutForm
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
+			this.lblAbout = new System.Windows.Forms.Label();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lblAbout
+			// 
+			this.lblAbout.AutoSize = true;
+			this.lblAbout.Location = new System.Drawing.Point(92, 57);
+			this.lblAbout.Name = "lblAbout";
+			this.lblAbout.Size = new System.Drawing.Size(169, 39);
+			this.lblAbout.TabIndex = 0;
+			this.lblAbout.Text = "Лабораторная работа №1\r\nВыполнил студент группы ЗП01\r\nГиптенко Тимур Петрович";
+			this.lblAbout.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// btnExit
+			// 
+			this.btnExit.Location = new System.Drawing.Point(95, 115);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(166, 23);
+			this.btnExit.TabIndex = 1;
+			this.btnExit.Text = "Выход";
+			this.btnExit.UseVisualStyleBackColor = true;
+			// 
+			// AboutForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+			this.ClientSize = new System.Drawing.Size(358, 195);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.lblAbout);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "AboutForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "About form";
+			this.TopMost = true;
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblAbout;
+		private System.Windows.Forms.Button btnExit;
+	}
+}
+
---
+++ Laboratory1/AboutForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Laboratory1
+{
+	public partial class AboutForm : Form
+	{
+		public AboutForm()
+		{
+			InitializeComponent();
+
+			BindEvents();
+		}
+
+		/// <summary>
+		/// Привязка событий формы
+		/// </summary>
+		/// <remarks>
+		/// При привязке новых событий будет происходит расширение только этого метода, а не переполнения логики конструктора. Также реализация Single Responsibility
+		/// </remarks>
+		private void BindEvents()
+		{
+			btnExit.Click += BtnExit_Click;
+		}
+
+		/// <summary>
+		/// Закрывает форму при нажатии
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void BtnExit_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+	}
+}
